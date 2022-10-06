8a2eb0ed177b248c67c3dec701205cc21c00193e
Kirill Titov <titov.k66@gmail.com>
2022-10-06 20:55:34 +0400

    homework-6

--- lesson_1/js/script.js
+++ lesson_1/js/script.js
@@ -13,32 +13,78 @@ function service(url) {
     .then((res) => res.json())
 }
 
+service(GET_BASKET_GOODS_ITEMS).then((data) => {
+    debugger
+})
+
 function init() {
+    const CustomButton = Vue.component('custom-button', {
+        template:`
+        <button class="search-button" type="button" v-on:click="$emit('click')">
+            <slot></slot>
+        </button>
+        `
+    })
+    const basketGoods = Vue.component('basket-goods', {
+        data() {
+            return {
+                basketGoodsItems: []
+            }
+        },
+
+        template:`
+        <div class="fixed-area">
+            <div class="basket-card">
+                <div class="basket-card__header">
+                    <h1 class="basket-card__header__title">basket card</h1>
+                    <div class="basket-card__header__delete-icon" v-on:click="$emit('closeclick')"></div>
+                </div>
+                <div class="basket-card__content">
+                    content
+                </div>
+            </div>
+        </div>
+        `
+    })
+
+    const goodsItem = Vue.component('goods-item', {
+        props: [
+            'item'
+        ],
+        template:`
+        <div class="goods-item">
+            <h3>{{ item.product_name }}</h3>
+            <p>{{ item.price }}</p>
+        </div>
+        `
+    })
+
     const app = new Vue({
         el: '#root',
         data: {
             items: [],
             filteredItems: [],
             search: '',
-            isVisibleCart: false
+            cardIsVision: false
         },
         methods: {
+            setVisionCard() {
+                this.cardIsVision = !this.cardIsVision
+            },
             fetchGoods() {
-                setTimeout (() => {
-                    service(GET_GOODS_ITEMS).then((data) => {
+                service(GET_GOODS_ITEMS).then((data) => {
                         this.items = data;
                         this.filteredItems = data;
                 });
-                }, 2000)
             },
             filterItems() {
                 this.filteredItems = this.items.filter(({ product_name }) => {
                     return product_name.match(new RegExp(this.search, 'gui'))
                 })
             },
-            setVisibleCard() {
-                this.isVisibleCart = !this.isVisibleCart;
-            }
+            // setVisibleCard() {
+            //     this.isVisibleCart = !this.isVisibleCart;
+            // }
         },
         computed: {
             calculatePrice() {
