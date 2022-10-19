8fe23d0839661ff3e2dd56c786848e775afca45f
Kirill Titov <titov.k66@gmail.com>
2022-10-19 10:42:30 +0400

    homework-8

--- lesson_1/js/script.js
+++ lesson_1/js/script.js
@@ -94,45 +94,4 @@ function init() {
 }
 window.onload = init
 
-// class GoodsItem {
-//     constructor({ product_name, price }) {
-//         this.product_name = product_name;
-//         this.price = price;
-//     }
-    
-//     render() {
-//         return `
-//         <div class="goods-item">
-//             <h3>${this.product_name}</h3>
-//             <p>${this.price}</p>
-//         </div>
-//     `;
-//     }
-// }
 
-// class GoodsList {
-//     items = [];
-//     fetchGoods() {
-//        return service(GET_GOODS_ITEMS).then((data) => {
-//             this.items = data;
-//         });
-//     }
-//     calculatePrice() {
-//         return this.items.reduce((prev, { price }) => {
-//             return prev + price;
-//         }, 0)
-//     }
-//     render() {
-//         const goods = this.items.map(item => {
-//             const goodItem = new GoodsItem(item);
-//             return goodItem.render()
-//         }).join('');
-
-
-//         document.querySelector('.goods-list').innerHTML = goods;
-//     }
-// }
-// const goodsList = new GoodsList();
-// goodsList.fetchGoods().then(() => {
-//     goodsList.render()
-// })
